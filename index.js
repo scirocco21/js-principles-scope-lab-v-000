4ee5ec36c08e0ac23a1ebb5ccd8f7b96720b9a53
Seb Gertz <sebgertz@gmail.com>
2018-05-06 00:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = "bob";
 function upperCaseCustomerName() {
-  return customerName.toUpperCase()
+ customerName.toUpperCase()
 }
 
 function setBestCustomer() {
